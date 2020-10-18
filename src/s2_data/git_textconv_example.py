5da8a669d9c3eb41c831dc01f110fda11a175c97
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2020-10-18 23:02:04 +0200

    Introduce consistent keys to category&descriptor

--- src/s2_data/git_textconv_example.py
+++ src/s2_data/git_textconv_example.py
@@ -7,15 +7,15 @@ import sys
 with io.open(sys.argv[1], 'rb') as save_file:
     save = save_file.read()
 
-    for category, elements in field_descriptors.items():
-        print('##', category, end='\n\n')
+    for category_key, category in field_descriptors.items():
+        print('##', category.name, end='\n\n')
 
-        for element in elements:
-            field_start = element.offset
-            field_end = element.offset + element.type.size
+        for dkey, descriptor in category.fields.items():
+            field_start = descriptor.offset
+            field_end = descriptor.offset + descriptor.type.size
 
-            value = element.type.from_binary(save[field_start:field_end])
+            value = descriptor.type.from_binary(save[field_start:field_end])
 
-            print(f'{element.name:30} =', value)
+            print(f'{descriptor.name:30} =', value)
 
         print(end='\n\n')
