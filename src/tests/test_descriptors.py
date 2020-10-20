7589e6afef44c339df990a2ac4d12e4c579141f8
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2020-10-20 22:19:07 +0200

    Add test to ensure fields don't overlap

--- src/tests/test_descriptors.py
+++ src/tests/test_descriptors.py
@@ -1,6 +1,7 @@
 from unittest import TestCase, main
 import s2_data
 import re
+from itertools import chain
 
 
 dict_key_regex = re.compile('^[a-z][a-z0-9_]*$')
@@ -16,6 +17,22 @@ class DescriptorsTestCase(TestCase):
         for category in s2_data.field_descriptors.values():
             for key in category.fields.keys():
                 self.assertTrue(dict_key_regex.match(key), key)
+
+    def test_descriptors_dont_overlap(self):
+        """Fields should never share bytes."""
+        categories = s2_data.field_descriptors.values()
+        descriptors = list(chain(*[c.fields.values() for c in categories]))
+        # Order by offset so we only have to check whether a field overlaps
+        # with the following field.
+        by_offset = sorted(descriptors, key=lambda d: d.offset)
+
+        current = by_offset[0]
+        for next_ in by_offset[1:]:
+            with self.subTest(current=current, next_=next_):
+                current_end = current.offset + current.type.size
+                self.assertLessEqual(current_end, next_.offset)
+
+                current = next_
 
 
 if __name__ == '__main__':
