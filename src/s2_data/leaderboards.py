45ef66d00d627abe5052b016d53eb87267eacf34
Gary M. Josack <gary@byoteki.com>
2021-03-03 00:06:55 -0800

    fixes

--- src/s2_data/leaderboards.py
+++ src/s2_data/leaderboards.py
@@ -14,7 +14,7 @@ NAME_LENGTH = 33
 
 
 @dataclass
-class Score:
+class Run:
     id: int = None
     name: str = None
     platform: int = None
@@ -27,58 +27,57 @@ class Score:
     block7: bytes = None
 
 
-def get_scores(compressed_data_handle):
+def get_runs(compressed_data_handle):
 
-    scores = []
+    runs = []
 
     dctx = zstandard.ZstdDecompressor()
     with dctx.stream_reader(compressed_data_handle) as reader:
 
         # We skip the first two entries for each array as they're always empty
-        num_scores = int.from_bytes(reader.read(8), "little") - 2
+        num_runs = int.from_bytes(reader.read(8), "little") - 2
 
-        for _ in range(num_scores):
-            scores.append(Score())
+        for _ in range(num_runs):
+            runs.append(Run())
 
         reader.seek(ID_POSITION + 2 * 8)
-        for idx in range(num_scores):
-            scores[idx].id = int.from_bytes(reader.read(8), "little")
+        for idx in range(num_runs):
+            runs[idx].id = int.from_bytes(reader.read(8), "little")
 
         reader.seek(NAMES_POSITION + 2 * NAME_LENGTH)
-        for idx in range(num_scores):
-            scores[idx].name = (
-                reader.read(NAME_LENGTH).partition(b"\x00")[0].decode("utf-8")
-            )
+        for idx in range(num_runs):
+            data = reader.read(NAME_LENGTH).partition(b"\x00")[0]
+            runs[idx].name = data.decode("utf-8", errors="replace")
 
         reader.seek(META_POSITION + 2 * 2)
-        for idx in range(num_scores):
-            scores[idx].platform = int.from_bytes(reader.read(1), "little")
-            scores[idx].character = int.from_bytes(reader.read(1), "little")
+        for idx in range(num_runs):
+            runs[idx].platform = int.from_bytes(reader.read(1), "little")
+            runs[idx].character = int.from_bytes(reader.read(1), "little")
 
         reader.seek(RUNDATA_POSITION + 2 * 8)
-        for idx in range(num_scores):
-            scores[idx].frames = int.from_bytes(reader.read(4), "little")
-            scores[idx].ending = int.from_bytes(reader.read(4), "little")
+        for idx in range(num_runs):
+            runs[idx].frames = int.from_bytes(reader.read(4), "little")
+            runs[idx].ending = int.from_bytes(reader.read(4), "little")
 
         reader.seek(SCORES_POSITION + 2 * 8)
-        for idx in range(num_scores):
-            scores[idx].score = int.from_bytes(reader.read(4), "little")
-            scores[idx].level = int.from_bytes(reader.read(4), "little")
+        for idx in range(num_runs):
+            runs[idx].score = int.from_bytes(reader.read(4), "little")
+            runs[idx].level = int.from_bytes(reader.read(4), "little")
 
         reader.seek(BLOCK6_POSITION + 2 * 4)
-        for idx in range(num_scores):
-            scores[idx].block6 = reader.read(4)
+        for idx in range(num_runs):
+            runs[idx].block6 = reader.read(4)
 
         reader.seek(BLOCK7_POSITION + 2 * 4)
-        for idx in range(num_scores):
-            scores[idx].block7 = reader.read(4)
+        for idx in range(num_runs):
+            runs[idx].block7 = reader.read(4)
 
-    return scores
+    return runs
 
 
 def main():
     import sys
 
     with open(sys.argv[1], "rb") as fh:
-        for score in get_scores(fh):
+        for score in get_runs(fh):
             print(score)
